249f4fc8ab7a610aa36ebf3c2f4391868e37e400
Christian Collado <colladoc2@newpaltz.edu>
2022-12-03 19:02:48 -0500

    Added Login System

--- Project2/src/main.ts
+++ Project2/src/main.ts
@@ -12,3 +12,24 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJa1SPjo1Wm0ZgYXZnmdmvoqs_hHlmiXU",
+  authDomain: "vue-firebase-auth-bafc4.firebaseapp.com",
+  projectId: "vue-firebase-auth-bafc4",
+  storageBucket: "vue-firebase-auth-bafc4.appspot.com",
+  messagingSenderId: "728860345284",
+  appId: "1:728860345284:web:b163b8b54d1f40a5c15a91",
+};
+
+const data = initializeApp(firebaseConfig);
+
+export { data }
+
+
+
